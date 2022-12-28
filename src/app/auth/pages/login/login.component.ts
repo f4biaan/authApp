2994cd913fcd5ac20a59c482b912a428874528ea
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-27 23:12:34 -0500

    Pantalla login y registro

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LoginComponent {
 
+  miFormulario: FormGroup = this.fb.group({
+    email: ['test1@test.com', [Validators.required, Validators.email]],
+    password: ['1234546', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  login() {
+    console.log(this.miFormulario.value);
+    console.log(this.miFormulario.valid);
+  }
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegisterComponent {
 
+  miFormulario: FormGroup = this.fb.group({
+    name: ['Test 1', [Validators.required]],
+    email: ['test1@test.com', [Validators.required, Validators.email]],
+    password: ['1234546', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  login() {
+    console.log(this.miFormulario.value);
+    console.log(this.miFormulario.valid);
+  }
+
+  registro() {}
 }
