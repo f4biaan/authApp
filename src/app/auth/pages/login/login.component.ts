ba1148dd7ea7514032034d46b6a3139a3c64ae78
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-29 20:55:17 -0500

    Login de usuario desde angular con el backend en node

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,17 +9,25 @@ import { Router } from '@angular/router';
   styles: [],
 })
 export class LoginComponent {
-
   miFormulario: FormGroup = this.fb.group({
     email: ['test1@test.com', [Validators.required, Validators.email]],
-    password: ['1234546', [Validators.required, Validators.minLength(6)]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
 
-  constructor(private fb: FormBuilder, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   login() {
     console.log(this.miFormulario.value);
+    const { email, password } = this.miFormulario.value;
+    console.log(email, password);
 
-    this.router.navigateByUrl('/dashboard');
+    this.authService.login(email, password).subscribe(resp => {
+      console.log(resp);
+    });
+    // this.router.navigateByUrl('/dashboard');
   }
 }
