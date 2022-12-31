edd90e899aad7312724333b2fb7974513b6cba54
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-31 11:32:02 -0500

    Validar token - guards

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 import { AuthResponse, Usuario } from '../interfaces/interfaces';
-import { catchError, map, of, tap } from 'rxjs';
+import { catchError, map, of, tap, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -37,12 +37,24 @@ export class AuthService {
     );
   }
 
-  validarToken() {
+  validarToken(): Observable<boolean> {
     const url = `${this.baseUrl}/auth/renew`;
     const headers = new HttpHeaders().set(
       'x-token',
       localStorage.getItem('token') || ''
     );
-    return this.http.get(url, { headers });
+    return this.http.get<AuthResponse>(url, { headers }).pipe(
+      map((resp) => {
+        console.log(resp.token);
+        localStorage.setItem('token', resp.token!);
+        this._usuario = {
+          name: resp.name!,
+          uid: resp.uid!,
+        };
+
+        return resp.ok;
+      }),
+      catchError((err) => of(false))
+    );
   }
 }
